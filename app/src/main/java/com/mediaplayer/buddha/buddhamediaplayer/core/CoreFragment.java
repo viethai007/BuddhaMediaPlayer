470c2edd3d8c83d247f7aaa3b785f1f5217b3039
Lukaz Mendoza <transporter2047@gmail.com>
2015-03-17 09:20:02 +0000

    Add core activity and fragment. Add fragment pager.

---
+++ app/src/main/java/com/mediaplayer/buddha/buddhamediaplayer/core/CoreFragment.java
@@ -0,0 +1,38 @@
+package com.mediaplayer.buddha.buddhamediaplayer.core;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mediaplayer.buddha.buddhamediaplayer.R;
+
+public abstract class CoreFragment extends Fragment {
+
+    public CoreFragment() {
+    }
+
+    protected abstract int getLayoutId();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        int layoutId = getLayoutId();
+        View view = inflater.inflate(layoutId, null, false);
+        Init(view);
+        LoadData();
+        UpdateUI();
+        BindEvent();
+        return view;
+    }
+
+    protected abstract void Init(View view);
+
+    protected abstract void LoadData();
+
+    protected abstract void UpdateUI();
+
+    protected abstract void BindEvent();
+}
